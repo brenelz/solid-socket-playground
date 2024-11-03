c5772e2ca8c97bc93962a745328ca26521916c00
Brenley Dueck <brenleydueck@gmail.com>
2024-11-02 21:42:16 -0500

    Try deno deploy

--- app.config.ts
+++ app.config.ts
@@ -3,6 +3,6 @@ import { client, router } from "./socket";
 
 export default defineConfig({
     ssr: false,
-    server: { experimental: { websocket: true } },
+    server: { experimental: { websocket: true }, preset: 'deno-deploy' },
     vite: { plugins: [client()] },
 }).addRouter(router);
