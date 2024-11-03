b8a987044056dfb09d86d48a346cf8eeb395f606
Brenley Dueck <brenleydueck@gmail.com>
2024-11-03 09:47:41 -0600

    Add drizzle

---
+++ src/lib/db.ts
@@ -0,0 +1,28 @@
+import { drizzle } from 'drizzle-orm/libsql';
+import { createClient } from '@libsql/client';
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+import { sql } from "drizzle-orm";
+
+const client = createClient({
+    url: process.env.TURSO_CONNECTION_URL!,
+    authToken: process.env.TURSO_AUTH_TOKEN!,
+});
+
+export const db = drizzle(client);
+
+export const usersTable = sqliteTable('users', {
+    id: text('id'),
+    username: text('username'),
+    email: text('email'),
+    avatar: text('avatar'),
+    name: text('name'),
+});
+
+export const notificationsTable = sqliteTable('notifications', {
+    id: integer('id', { mode: 'number' }).primaryKey({ autoIncrement: true }),
+    userId: text('userId').notNull(),
+    issueId: text('issueId'),
+    title: text('title'),
+    createdAt: text("timestamp").default(sql`(CURRENT_TIMESTAMP)`),
+    readAt: text("timestamp")
+});
