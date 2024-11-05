ee29ec9806939253e9a8b68652ecc30d993182fb
Brenley Dueck <brenleydueck@gmail.com>
2024-11-04 22:57:35 -0600

    Maybe this works

--- src/lib/socket-chat.ts
+++ src/lib/socket-chat.ts
@@ -1,33 +1,36 @@
 "use socket";
 
-import { createEffect, createResource, createSignal } from "solid-js";
+import { openKv } from "@deno/kv";
+import { createEffect, createResource } from "solid-js";
 import { createSocketMemo } from "../../socket/lib/shared";
 import { db, messagesTable } from "./db";
+
+const kv = await openKv("https://api.deno.com/databases/a53bab8f-3ab2-45c9-ba85-f8d9a8db5375/connect");
 
 async function getMessages() {
     return db.select().from(messagesTable);
 }
-
-const [triggerUpdate, setTriggerUpdate] = createSignal(false);
 
 const addMessage = async (props: { name: string, message: string }) => {
     await db.insert(messagesTable).values({
         name: props.name,
         message: props.message
     })
-    setTriggerUpdate(true);
-
+    await kv.set(["triggerUpdate"], true);
 }
 
 export const useSocketChat = () => {
     const [messages, { refetch }] = createResource(() => getMessages());
 
     createEffect(() => {
-        refetch();
-        if (triggerUpdate()) {
-            setTriggerUpdate(false);
+        async function watch() {
+            for await (const [triggerUpdate] of kv.watch([["triggerUpdate"]])) {
+                refetch();
+            }
         }
-    });
+        watch();
+    })
+
 
     return {
         messages: createSocketMemo(messages),
