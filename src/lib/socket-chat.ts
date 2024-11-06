76944a852d2af973ffc79922e192503cb302f14b
Brenley Dueck <brenleydueck@gmail.com>
2024-11-06 17:43:54 -0600

    Signal instead of kv.watch

--- src/lib/socket-chat.ts
+++ src/lib/socket-chat.ts
@@ -1,11 +1,12 @@
 "use socket";
 
-import { createEffect, createResource } from "solid-js";
+import { createEffect, createResource, createSignal } from "solid-js";
 import { createSocketMemo } from "../../socket/lib/shared";
 import { db, messagesTable } from "./db";
-import { useKv } from "./useKv";
+//import { useKv } from "./useKv";
 
-const kv = await useKv("https://api.deno.com/databases/a53bab8f-3ab2-45c9-ba85-f8d9a8db5375/connect");
+//const kv = await useKv("https://api.deno.com/databases/a53bab8f-3ab2-45c9-ba85-f8d9a8db5375/connect");
+const [triggerUpdate, setTriggerUpdate] = createSignal(false);
 
 async function getMessages() {
     return db.select().from(messagesTable);
@@ -16,19 +17,17 @@ const addMessage = async (props: { name: string, message: string }) => {
         name: props.name,
         message: props.message
     })
-    await kv.set(["triggerUpdate"], true);
+    //await kv.set(["triggerUpdate"], true);
+    setTriggerUpdate(true);
 }
 
 export const useSocketChat = () => {
     const [messages, { refetch }] = createResource(() => getMessages());
 
     createEffect(() => {
-        async function watch() {
-            for await (const [_] of kv.watch([["triggerUpdate"]])) {
-                refetch();
-            }
+        if (triggerUpdate()) {
+            refetch();
         }
-        watch();
     })
 
 
